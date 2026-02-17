5948dbcb159636e5ad37951afe509a18437d78ed
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-16 23:10:36 -0500

    Create

---
+++ skip_multiples_of_5.py
@@ -0,0 +1,11 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 3 Assignment 4
+'''
+for i in range(1, 51):
+    if i % 5 == 0:
+       # The continue statement is used to skip the current iteration of the loop when i is a multiple of 5.
+       continue
+    print(i)
